d87de98616423ad482e614ce3863982b5a6425c0
Maurits van Beusekom <maurits@vnbskm.nl>
2019-10-01 14:16:48 +0200

    Added unit-test to handle AppStarted event

--- test/units/app/authentication/bloc/authentication_bloc_test.dart
+++ test/units/app/authentication/bloc/authentication_bloc_test.dart
@@ -28,16 +28,96 @@ void main() {
     authenticationBloc = null;
   });
 
-  group('initialState tests', () {
+  group('when \'AuthenticationBloc\' is constructed', () {
     test('initialState should be `Uninitialized`', () {
       expect(authenticationBloc.initialState, Uninitialized());
     });
   });
 
-  group('AppStarted event tests', () {});
+  group('when receiving \'AppStarted\' event', () {
+    test('should call \'isSignedIn\' method on UserRepository exactly once.', () {
+      final event = AppStarted();
+      final user = User('Mr. Example', 'test@example.com');
 
-  group('LoggedIn event tests', () {
-    test('emits [Uninitialized, Authenticated] when logging in', () {
+      expectLater(authenticationBloc.state, mayEmit(Authenticated(user.name)))
+          .then((_) => verify(mockUserRepository.isSignedIn()).called(1));
+
+      authenticationBloc.dispatch(event);
+    });
+
+    test('should emit [Uninitialized, Authenticated] if user is already logged in.', () {
+      final event = AppStarted();
+      final user = User('Mr. Example', 'test@example.com');
+      final expected = [
+        Uninitialized(),
+        Authenticated(user.name),
+      ];
+
+      when(mockUserRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+      when(mockUserRepository.getUser()).thenAnswer((_) => Future.value(user));
+
+      expectLater(authenticationBloc.state, emitsInOrder(expected));
+
+      authenticationBloc.dispatch(event);
+    });
+
+    test('should call \'getUser\' method on UserRepository if user is already logged in.', () {
+      final event = AppStarted();
+      final user = User('Mr. Example', 'test@example.com');
+
+      when(mockUserRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+      when(mockUserRepository.getUser()).thenAnswer((_) => Future.value(user));
+
+      expectLater(authenticationBloc.state, mayEmit(Authenticated(user.name)))
+          .then((_) => verify(mockUserRepository.getUser()).called(1));
+
+      authenticationBloc.dispatch(event);
+    });
+
+    test('should emit [Uninitialized, Unauthenticated] when user is not logged in.', () {
+      final event = AppStarted();
+      final expected = [
+        Uninitialized(),
+        Unauthenticated(),
+      ];
+
+      when(mockUserRepository.isSignedIn()).thenAnswer((_) => Future.value(true));
+
+      expectLater(authenticationBloc.state, emitsInOrder(expected));
+
+      authenticationBloc.dispatch(event);
+    });
+
+    test('should not call \'getUser\' method on UserRepository if user is not logged in.', () {
+      final event = AppStarted();
+      final user = User('Mr. Example', 'test@example.com');
+
+      when(mockUserRepository.isSignedIn()).thenAnswer((_) => Future.value(false));
+      when(mockUserRepository.getUser()).thenAnswer((_) => Future.value(user));
+
+      expectLater(authenticationBloc.state, mayEmit(Authenticated(user.name)))
+          .then((_) => verifyNever(mockUserRepository.getUser()));
+
+      authenticationBloc.dispatch(event);
+    });
+
+    test('should emit [Uninitialized, Unauthenticated] if an exception is thrown.', () {
+      final event = AppStarted();
+      final expected = [
+        Uninitialized(),
+        Unauthenticated(),
+      ];
+
+      when(mockUserRepository.isSignedIn()).thenThrow(Exception());
+      
+      expectLater(authenticationBloc.state, emitsInOrder(expected));
+
+      authenticationBloc.dispatch(event);
+    });
+  });
+
+  group('when receiving \'LoggedIn\' event', () {
+    test('should emit [Uninitialized, Authenticated] when logging in', () {
       final event = LoggedIn();
       final user = User('Mr. Example', 'test@example.com');
       final expected = [
@@ -52,7 +132,7 @@ void main() {
       authenticationBloc.dispatch(event);
     });
 
-    test('calls \'getUser\' method on the supplied UserRepository exactly once', () {
+    test('should call \'getUser\' method on the supplied UserRepository exactly once', () {
       final event = LoggedIn();
       final user = User('Mr. Example', 'test@example.com');
 
@@ -65,8 +145,8 @@ void main() {
     });
   });
 
-  group('LoggedOut event tests', () {
-    test('emits [Uninitialized, Unauthenticated] when logging out', () {
+  group('when receiving \'LoggedOut\' event', () {
+    test('should emit [Uninitialized, Unauthenticated].', () {
       final event = LoggedOut();
       final expected = [
         Uninitialized(),
@@ -78,7 +158,7 @@ void main() {
       authenticationBloc.dispatch(event);
     });
 
-    test('calls \'signOut\' method on the supplied UserRepository exactly once', () {
+    test('should call \'signOut\' method on the supplied UserRepository exactly once.', () {
       final event = LoggedOut();
 
       expectLater(authenticationBloc.state, mayEmit(Unauthenticated()))

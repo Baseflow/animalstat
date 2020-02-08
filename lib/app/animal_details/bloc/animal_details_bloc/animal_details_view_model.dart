9c51224b1c8e1fe3ed479e3fba00168d3950c5a6
Maurits van Beusekom <maurits@baseflow.com>
2020-02-08 21:48:48 +0100

    Moved view model to better location

---
+++ lib/app/animal_details/bloc/animal_details_bloc/animal_details_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:livestock_repository/livestock_repository.dart';
+import 'package:intl/intl.dart';
+
+class AnimalDetailsViewModel {
+  final int animalNumber;
+  final String dateOfBirth;
+  final HealthStates currentHealthStatus;
+
+  AnimalDetailsViewModel.fromModel(Animal animal)
+      : this.animalNumber = animal.animalNumber,
+        this.dateOfBirth = DateFormat('dd-MM-yyyy').format(animal.dateOfBirth),
+        this.currentHealthStatus = animal.currentHealthStatus;
+}
